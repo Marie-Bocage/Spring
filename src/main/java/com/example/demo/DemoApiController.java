534551e790f544382fdc5ceb1eda1d12c799a4b7
Marie Bocage <marie.bocage@gmail.com>
2023-01-03 11:50:09 +0100

    Ajout personne bd via POST

--- src/main/java/com/example/demo/DemoApiController.java
+++ src/main/java/com/example/demo/DemoApiController.java
@@ -9,26 +9,34 @@ import java.util.List;
 @RequestMapping("api")
 public class DemoApiController {
 
-    @Autowired //(injecte un objet dans une variable)
-    AnnuaireService annuaireService;// = new AnnuaireService();
-
-    @GetMapping("personnes")
-    public List<Personne> getAllPersonne(){
-//        Personne p = new Personne("Alain", "Delon");
-//        return p;
-        return annuaireService.getPersonnes();
-    }
-
-    @GetMapping("personnes/{id}")
-    public Personne getOnePersonne(@PathVariable Integer id){
-        return annuaireService.getOnePersonne(id);
-    }
+    @Autowired
+    AnnuaireDatabaseService annuaireDatabaseService;
 
     @PostMapping("personnes")
     public void createPersonne(@RequestBody Personne personne){
-//        System.out.println(personne);
-        annuaireService.addPersonne(personne);
+        annuaireDatabaseService.addPersonne(personne);
     }
+
+//    @Autowired //(injecte un objet dans une variable)
+//    AnnuaireService annuaireService;// = new AnnuaireService();
+
+//    @GetMapping("personnes")
+//    public List<Personne> getAllPersonne(){
+////        Personne p = new Personne("Alain", "Delon");
+////        return p;
+//        return annuaireService.getPersonnes();
+//    }
+//
+//    @GetMapping("personnes/{id}")
+//    public Personne getOnePersonne(@PathVariable Integer id){
+//        return annuaireService.getOnePersonne(id);
+//    }
+//
+//    @PostMapping("personnes")
+//    public void createPersonne(@RequestBody Personne personne){
+////        System.out.println(personne);
+//        annuaireService.addPersonne(personne);
+//    }
 
 //    @PutMapping("")
 
