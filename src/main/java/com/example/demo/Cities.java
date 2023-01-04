35886373e8a1d2923299bb2fab2065f4ce4cdf93
Marie Bocage <marie.bocage@gmail.com>
2023-01-04 10:50:22 +0100

    Ajout colonne citizensNb

--- src/main/java/com/example/demo/Cities.java
+++ src/main/java/com/example/demo/Cities.java
@@ -14,13 +14,16 @@ public class Cities {
 
     private String country;
 
+    private int citizensNb;
+
     public Cities() {
     }
 
-    public Cities(Integer id, String city, String country) {
+    public Cities(Integer id, String city, String country, int citizensNb) {
         this.id = id;
         this.city = city;
         this.country = country;
+        this.citizensNb = citizensNb;
     }
 
     public Integer getId() {
@@ -47,12 +50,21 @@ public class Cities {
         this.country = country;
     }
 
+    public int getCitizensNb() {
+        return citizensNb;
+    }
+
+    public void setCitizensNb(int citizensNb) {
+        this.citizensNb = citizensNb;
+    }
+
     @Override
     public String toString() {
         return "Cities{" +
                 "id=" + id +
                 ", city='" + city + '\'' +
                 ", country='" + country + '\'' +
+                ", citizensNb=" + citizensNb +
                 '}';
     }
 }
