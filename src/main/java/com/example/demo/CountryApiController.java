60f59c2085ee89172030fca542708a4f07b782e9
Marie Bocage <marie.bocage@gmail.com>
2023-01-04 14:37:41 +0100

    Connexion base de données et Api

--- src/main/java/com/example/demo/CountryApiController.java
+++ src/main/java/com/example/demo/CountryApiController.java
@@ -1,9 +1,20 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+@RestController
+@RequestMapping("api")
 public class CountryApiController {
 
     @Autowired
     CountryDatabaseService countryDatabaseService;
+
+    @GetMapping("games")
+    public List<Country> getAllCountries(){
+        return countryDatabaseService.findAllCountries();
+    }
 }
--- src/main/java/com/example/demo/CountryDatabaseService.java
+++ src/main/java/com/example/demo/CountryDatabaseService.java
@@ -1,9 +1,17 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CountryDatabaseService {
 
     @Autowired
     CountryRepository countryRepository;
+
+    public List<Country> findAllCountries(){
+        return countryRepository.findAll();
+    }
 }
