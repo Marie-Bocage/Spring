f866fc6c7abe223d3c3b908400c1373b9617577e
Marie Bocage <marie.bocage@gmail.com>
2023-01-04 14:49:05 +0100

    Ajout methode random pour GET

--- src/main/java/com/example/demo/CountryApiController.java
+++ src/main/java/com/example/demo/CountryApiController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Random;
+
 @RestController
 @RequestMapping("api")
 public class CountryApiController {
@@ -14,7 +16,11 @@ public class CountryApiController {
     CountryDatabaseService countryDatabaseService;
 
     @GetMapping("games")
-    public List<Country> getAllCountries(){
-        return countryDatabaseService.findAllCountries();
+    public Country getRandomCountry(){
+        List<Country> countries = countryDatabaseService.findAllCountries();
+
+        Random rand = new Random();
+        Country randomCountry = countries.get(rand.nextInt(countries.size()));
+        return randomCountry;
     }
 }
