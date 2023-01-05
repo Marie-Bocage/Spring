53d24025e03bc7074693988d53c9b54fbc08af3a
Marie Bocage <marie.bocage@gmail.com>
2023-01-05 14:49:17 +0100

    Ajout POST chien

--- src/main/java/com/example/demo/DemoSessionController.java
+++ src/main/java/com/example/demo/DemoSessionController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +12,12 @@ public class DemoSessionController {
     @Autowired
     HttpSession httpSession;
 
+    @Autowired
+    AnnuaireDatabaseService annuaireDatabaseService;
+
+    @Autowired
+    ChienDatabaseService chienDatabaseService;
+
     @PostMapping("session")
     public void writeSession(@RequestBody Personne personne) {
         httpSession.setAttribute("moi", personne);
@@ -20,5 +27,11 @@ public class DemoSessionController {
     public Personne readSession() {
         Personne personne = (Personne)httpSession.getAttribute("moi");
         return personne;
+    }
+
+    @PostMapping("chien/{id}")
+    public void newDog(@RequestBody Chien c, @PathVariable Integer id) {
+        c.setMaitre(annuaireDatabaseService.getPersonneById(id).get());
+        chienDatabaseService.addChien(c);
     }
 }
