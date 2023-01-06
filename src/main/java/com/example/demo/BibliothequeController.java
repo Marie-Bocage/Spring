d0380427d65ca04b43ea7e99859630508d9ce3f2
Marie Bocage <marie.bocage@gmail.com>
2023-01-06 09:18:33 +0100

    Ajout POST categorie

--- src/main/java/com/example/demo/BibliothequeController.java
+++ src/main/java/com/example/demo/BibliothequeController.java
@@ -1,4 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api")
 public class BibliothequeController {
+
+    @Autowired
+    BibliothequeDatabaseService bibliothequeDatabaseService;
+
+    @PostMapping("categorie")
+    public void createCategorie(@RequestBody Categorie categorie) {
+        bibliothequeDatabaseService.addCategorie(categorie);
+    }
 }
